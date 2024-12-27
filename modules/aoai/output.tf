934ad9611494a0fe9c5159de6b9512940dffb28e
SPShota <check.it.check.it.boom@gmail.com>
2024-12-27 11:40:58 +0900

    fix: align output value formatting in aoai module

--- modules/aoai/output.tf
+++ modules/aoai/output.tf
@@ -1,9 +1,9 @@
 output "gpt_4o_sku_name" {
-  value = azurerm_cognitive_deployment.gpt_4o.sku[0].name
+  value       = azurerm_cognitive_deployment.gpt_4o.sku[0].name
   description = "The SKU name of the GPT-4o OpenAI model"
 }
 
 output "gpt_4o_mini_sku_name" {
-  value = azurerm_cognitive_deployment.gpt_4o_mini.sku[0].name
+  value       = azurerm_cognitive_deployment.gpt_4o_mini.sku[0].name
   description = "The SKU name of the GPT-4o-mini OpenAI model"
 }
